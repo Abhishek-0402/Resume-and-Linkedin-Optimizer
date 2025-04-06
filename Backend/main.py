55c66b5eaadeb412c1031be3f0ec934683d3371e
Abhishek Maurya <abhishek2504maurya@gmail.com>
2025-04-05 22:21:44 -0400

    Fix crash by adding python-multipart

--- Backend/main.py
+++ Backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Replace with frontend URL in production
+    allow_origins=["*"],  # Replace with frontend URL in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
